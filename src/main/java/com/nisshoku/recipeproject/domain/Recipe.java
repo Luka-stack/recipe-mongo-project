a3c2f06a375742a0e09680bb4dfa869d989096ca
takacchi <uchiha.takahiro@gmail.com>
2018-11-24 20:40:17 +0100

    Added 'Difficulty' enum and new field in Recipe model

--- src/main/java/com/nisshoku/recipeproject/domain/Recipe.java
+++ src/main/java/com/nisshoku/recipeproject/domain/Recipe.java
@@ -17,8 +17,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //TODO add
-    // private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredientSet;
@@ -109,4 +110,19 @@ public class Recipe {
         this.notes = notes;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredientSet() {
+        return ingredientSet;
+    }
+
+    public void setIngredientSet(Set<Ingredient> ingredientSet) {
+        this.ingredientSet = ingredientSet;
+    }
 }
